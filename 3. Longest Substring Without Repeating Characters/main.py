a772dcadedb13763c46741f6830edbf37a93cd75
bensfiaraiddev-crypto <bensfiaraid.dev@gmail.com>
2026-06-08 00:28:42 +0100

    new consept learn sets
    using sets that are like lists but dont handle duplications i was able to create this system it works

--- 3. Longest Substring Without Repeating Characters/main.py
+++ 3. Longest Substring Without Repeating Characters/main.py
@@ -8,24 +8,18 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        top_l : int = 0
-        target : dict = {}
-        a:int = 0
-        while a<len(s) or s == "":
-            letter = s[a]
-            if letter in target:
-                l = len(target)
-                top_l = l if l>top_l else top_l
-                print(top_l)
-                s = s[target[letter]:]
-                target = {}
-                a = 0
-            else:
-                a += 1
-                target[letter] =a
-        top_l = len(target) if len(target)>top_l else top_l
-        return top_l
+        char_set = set()
+        max_lenght : int = 0
+        left = 0
+        for right in range(len(s)):
+            print(char_set)
+            if s[right] in char_set:
+                char_set.remove(s[right])
+                left +=1
 
-            
-   
-print(Solution().lengthOfLongestSubstring(input()))            
+            char_set.add(s[right])
+            max_lenght = max(max_lenght,right - left +1)
+        return max_lenght
+
+
+print(Solution().lengthOfLongestSubstring(input()))
