8ef49ccbc2cb6e13d3e351fd43fd961646d43d72
bensfiaraiddev-crypto <bensfiaraid.dev@gmail.com>
2026-06-07 16:17:34 +0100

    updating code it worked!!
    i  tried new method now i use a dectuatnry to know the 1st index of a duplicate but maybe i should use two barriers a start one and an end one instead ints are light right?

--- 3. Longest Substring Without Repeating Characters/main.py
+++ 3. Longest Substring Without Repeating Characters/main.py
@@ -8,10 +8,24 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        collction : list = []
-        for index,letter in  enumerate(s):
-            if not letter in  collction:
-                collction.append(letter)
-                
-        return len(collction)
+        top_l : int = 0
+        target : dict = {}
+        a:int = 0
+        while a<len(s) or s == "":
+            letter = s[a]
+            if letter in target:
+                l = len(target)
+                top_l = l if l>top_l else top_l
+                print(top_l)
+                s = s[target[letter]:]
+                target = {}
+                a = 0
+            else:
+                a += 1
+                target[letter] =a
+        top_l = len(target) if len(target)>top_l else top_l
+        return top_l
+
             
+   
+print(Solution().lengthOfLongestSubstring(input()))            
